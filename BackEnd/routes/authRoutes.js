caae4d78c208e6e1158d6c701415ea3c97da2072
JaeMoon96 <ljm96040@gmail.com>
2025-07-03 17:19:58 +0900

    Backend Sign Up / Login API v0.2 (user auth API (register, login, update, delete)

--- BackEnd/routes/authRoutes.js
+++ BackEnd/routes/authRoutes.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
 
-router.post('/register', register);
-router.post('/login', login);
+const { register,login,updateUser,deleteUser, getMe } = require("../controllers/authController");
+
+const verifyToken = require("../middlewares/authMiddleware");
+
+router.post("/register", register);           
+router.post("/login", login);                 
+router.put("/update", verifyToken, updateUser);   // PUT /update - 회원정보 수정 (JWT 필요)
+router.delete("/delete", verifyToken, deleteUser); // DELETE /delete - 회원탈퇴 (JWT 필요)
+router.get("/me", verifyToken, getMe);        // GET /me - 내 정보 조회 (JWT 필요)
 
 module.exports = router;
